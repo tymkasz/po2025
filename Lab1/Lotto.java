13c1127916092407f5c29edefd1e0ac76d54fd2a
tymkasz <ihkruk@gmail.com>
2025-10-06 19:58:31 +0200

    add Lotto.java file

---
+++ Lab1/Lotto.java
@@ -0,0 +1,13 @@
+import java.util.Random
+
+public class Lotto{
+    public static void main(String[] args){
+        
+        String[] numbers;
+        Radom generator = new Random();
+
+        for (int i = 0; i < 6; i++){
+            numbers.add(generator.nextDouble())
+        }
+    }
+}
