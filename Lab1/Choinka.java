f7e0c8f716150d7ea584d70f41589a0c7d5f2c7b
tymkasz <ihkruk@gmail.com>
2025-10-08 22:51:37 +0200

    ended Choinka.java

--- Lab1/Choinka.java
+++ Lab1/Choinka.java
@@ -7,11 +7,24 @@ public class Choinka{
         }
 
 		int height = Integer.valueOf(args[0]);
-		String txt = "*";
+		String star = "*";
+		String trunk = "|||";
+		String space = " ";
 
+		//printing stars
 		for (int i = 1; i <= height; i++){
 			
-			System.out.println(txt.repeat(i));
+			System.out.println((space.repeat(height-i)) + star.repeat(2*i-1));
 			}
+		
+		// printing a trunk
+		if (height > 3) {
+			for (int j = 1; j <= 2; j++) {
+				System.out.println(space.repeat(height - 2) + trunk);
+			}
+		} else if (height == 3) {
+			System.out.println(space.repeat(height - 2) + trunk);
+		}
+
 	}
 }
