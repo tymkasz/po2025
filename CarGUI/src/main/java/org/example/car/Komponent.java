95a5a9401b0f7ff842c8bca507807f51abc38fde
tymkasz <ihkruk@gmail.com>
2026-01-17 12:32:23 +0100

    add comments

--- CarGUI/src/main/java/org/example/car/Komponent.java
+++ CarGUI/src/main/java/org/example/car/Komponent.java
@@ -1,10 +1,13 @@
 package org.example.car;
 
+// Abstract -> nie mogę stworzyć instancji klasy Komponent -> służy ona jedynie dziedziczeniu
+// DRY - Don't Repeat Yourself
 public abstract class Komponent
 {
     protected String producent;
     protected String model;
 
+    // Konstruktor -> wymuszenie dziedziczenia producenta i modelu
     public Komponent(String producent, String model)
     {
         this.producent = producent;
