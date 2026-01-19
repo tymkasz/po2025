5fdd831abb86b6703f6bed6f4598b4b5418e8d85
tymkasz <ihkruk@gmail.com>
2026-01-19 11:46:08 +0100

    cleaning code Pozycja.java

--- CarGUI/src/main/java/org/example/car/Pozycja.java
+++ CarGUI/src/main/java/org/example/car/Pozycja.java
@@ -1,36 +1,28 @@
 package org.example.car;
 
-public class Pozycja
-{
-    double x;
-    double y;
+public class Pozycja {
+    private double x;
+    private double y;
 
-    // Przeciążenie -> dwa konstruktory (obiekty) w zależności od potrzeb użytkownika
-    // Konstruktor domyslny -> domyślna pozycja x=0.0 i y=0.0
-    public Pozycja()
-    {
-        this.x = 0.0;
-        this.y = 0.0;
+    // Konstruktor domyślny
+    public Pozycja() {
+        this(0.0, 0.0);
     }
 
     // Konstruktor z wstępnym położeniem podanym przez użytkownika
-    public Pozycja(int startX, int startY)
-    {
+    public Pozycja(double startX, double startY) {
         this.x = startX;
         this.y = startY;
     }
 
     // Modyfikacja położenia
-    public void updatePosition(double deltaX, double deltaY)
-    {
+    public void updatePosition(double deltaX, double deltaY) {
         this.x += deltaX;
         this.y += deltaY;
     }
 
-    // Uzyskanie położenia
-    public String getPosition()
-    {
-        return String.format("x: " + this.x + ", y: " + this.y);
-    }
+    // Gettery
+    public double getX() { return x; }
+    public double getY() { return y; }
 
 }
