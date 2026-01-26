33e1a314a0426ce8a53e590fc17721f48bd22638
tymkasz <ihkruk@gmail.com>
2026-01-26 11:29:16 +0100

    modify setters

--- CarGUI/src/main/java/org/example/car/Position.java
+++ CarGUI/src/main/java/org/example/car/Position.java
@@ -9,9 +9,11 @@ public class Position {
         this.y = y;
     }
 
-    public double getX() { return x; }
-    public double getY() { return y; }
+    public double getX() { return this.x; }
+    public double getY() { return this.y; }
 
-    public double setX(double x) { return x; }
-    public double setY(double y) { return y; }
+    public void setX(double x) { this.x = x; }
+    public void setY(double y) { this.y = y; }
+
+
 }
